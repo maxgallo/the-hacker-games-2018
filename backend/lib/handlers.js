f5b3a40d03757f97c208b5b6c5aac98e3845fb13
Andrea Falzetti <afalzetti@gmail.com>
2018-01-18 21:27:25 +0000

    backend: I am very tired

--- backend/lib/handlers.js
+++ backend/lib/handlers.js
@@ -1,26 +1,29 @@
 /**
  * Socket Handler for the 'connection' event
  */
-const connection = brain => async (socket) => {
+const connection = brain => socket => {
+  socket.on('START_EVENT', async payload => {
+    const questions = await brain.getQuestion({ query: { level: -1 } });
 
-  const messages = await brain.getQuestion({ level: -1 });
+    const sendMessage = (socket, event, payload, messageIndex) => {
+      setTimeout(() => {
+        console.log('messageIndex ===>', messageIndex);
+        socket.emit(event, JSON.stringify(payload));
+        console.log('emitting...', payload.message)
+      }, 1000 * messageIndex)
+    };
 
-  const sendMessage = (socket, event, payload, messageIndex) => {
-    setTimeout(() => {
-      socket.emit(event, JSON.stringify(payload));
-    }, 1000 * messageIndex)
-  };
-
-  let index = 0;
-  for (message in messages) {
-    sendMessage(socket, 'QUESTION', message, index++);
-  }
+    let index = 0;
+    for (let question of questions) {
+      sendMessage(socket, 'QUESTION', question, index++);
+    }
+  });
 
   socket.on('ANSWER', payload => {
     console.log(payload);
   });
-}
+};
 
-module.exports = (brain) => ({
+module.exports = brain => ({
   connection: connection(brain)
 });
