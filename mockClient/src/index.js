8c079a68e859d6983506651890266df73ef738bb
Patrick Kunka <patrick.kunka@dazn.com>
2018-01-18 17:32:03 +0000

    refactor client to avoid document

--- mockClient/src/index.js
+++ mockClient/src/index.js
@@ -1,14 +1,17 @@
 import ChatClient from './services/ChatClient';
 import * as Events from './constants/Events';
 
+const answersArea = document.querySelector('#answers');
+const questionArea = document.querySelector('#question');
+
 // Create new chat client with socket URL
 
 const chatClient = new ChatClient('https://aa329ee8.ngrok.io');
 
 // Listen for questions from the server
 
-chatClient.addEventListener(Events.QUESTION, ({detail}) => {
-    console.log('received question:', detail.question);
+chatClient.onQuestion(question => {
+    console.log('question received', question);
 });
 
 // Start the chat (e.g. when user opens app)
