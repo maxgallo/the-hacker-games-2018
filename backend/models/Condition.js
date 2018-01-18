12b09038ddc8a78a3afff9a6e79dfc6aabc1e7d1
Andrea Falzetti <afalzetti@gmail.com>
2018-01-18 19:27:02 +0000

    backend: update models

--- backend/models/Condition.js
+++ backend/models/Condition.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const collectionName = 'Condition';
+
 const schema = new Schema({
   message: String
 });
 
-const model = mongoose.model('Condition', schema)
+const model = mongoose.model(collectionName, schema, collectionName)
 
 module.exports = model;
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const collectionName = 'User';
+
 const schema = new Schema({
   name: String,
   email: String,
   password: String,
+  questions: [{ type: Schema.Types.ObjectId, ref: 'Question' }],
+  answers: [{ type: Schema.Types.ObjectId, ref: 'Answer' }]
 });
 
-const model = mongoose.model('User', schema)
+const model = mongoose.model(collectionName, schema, collectionName)
 
 module.exports = model;
