a1d6cee840056983e894ab90631698c5a821ce41
Patrick Kunka <patrick.kunka@dazn.com>
2018-01-18 17:54:45 +0000

    fix onQuestion callback

--- mockClient/src/index.js
+++ mockClient/src/index.js
@@ -4,14 +4,32 @@ import * as Events from './constants/Events';
 const answersArea = document.querySelector('#answers');
 const questionArea = document.querySelector('#question');
 
+answersArea.addEventListener('click', handleAnswerClick);
+
 // Create new chat client with socket URL
 
-const chatClient = new ChatClient('https://aa329ee8.ngrok.io');
+const chatClient = new ChatClient('https://fbef2b12.ngrok.io');
+
+function renderAnswerButtons(answers = []) {
+    console.log(answers);
+
+    return answers.reduce((html, answer) => html + `<button data-id="${answer.id}">${answer.text}</button>`, '');
+}
+
+function handleAnswerClick({target}) {
+    const answerId = target.getAttribute('data-id');
+
+    chatClient.selectAnswer(answerId);
+
+    renderAnswerButtons();
+}
 
 // Listen for questions from the server
 
 chatClient.onQuestion(question => {
-    console.log('question received', question);
+    questionArea.value = question.question;
+
+    renderAnswerButtons(question.answers);
 });
 
 // Start the chat (e.g. when user opens app)
