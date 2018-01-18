979ebc5c92da24a3d5dd6ac27c7faab95930b438
Andrea Falzetti <afalzetti@gmail.com>
2018-01-18 20:25:25 +0000

    backend: add chain of messages with a 1s delay

--- backend/lib/handlers.js
+++ backend/lib/handlers.js
@@ -3,15 +3,22 @@
  */
 const connection = brain => async (socket) => {
 
-  const question = await brain.getQuestion({ level: 0 });
-  console.log('sending question', question);
+  const messages = await brain.getQuestion({ level: -1 });
 
-  socket.emit('QUESTION', JSON.stringify(question));
+  const sendMessage = (socket, event, payload, messageIndex) => {
+    setTimeout(() => {
+      socket.emit(event, JSON.stringify(payload));
+    }, 1000 * messageIndex)
+  };
+
+  let index = 0;
+  for (message in messages) {
+    sendMessage(socket, 'QUESTION', message, index++);
+  }
+
   socket.on('ANSWER', payload => {
     console.log(payload);
   });
-
-
 }
 
 module.exports = (brain) => ({
