d9681aba7953625dc48c9155acc2ff14cedc490a
Max Gallo <max.gallo@dazn.com>
2018-01-18 16:31:49 +0000

    CLIENT: moves logic from component to reducers

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,5 +1,11 @@
+import randomNumber from '../random/randomNumber';
 
-const updateAction = (x, y, animationId, animationType) => ({
+const updateAction = (
+    x,
+    y,
+    animationId,
+    animationType
+) => ({
     type: 'updateAction',
     x,
     y,
@@ -7,6 +13,27 @@ const updateAction = (x, y, animationId, animationType) => ({
     animationType
 });
 
+
+function addBotMessageAction (
+    message,
+    replyOptions,
+    reply
+) {
+    const whatToReturn = {
+        type: 'addBotMessageAction',
+        messages: [],
+        replyOptions,
+    };
+
+    if (reply) {
+        whatToReturn.messages.push(reply);
+    }
+    whatToReturn.messages.push(message);
+
+    return whatToReturn;
+}
+
 export {
     updateAction,
+    addBotMessageAction,
 };
---
+++ client/reducers/animation.js
@@ -0,0 +1,45 @@
+const defaultState = {
+    diff: 0,
+    height: 0,
+};
+
+const animationReducer = (
+    state = defaultState,
+    action
+) => {
+    switch (action.type) {
+        case 'updateAction': {
+            const stateForAnimationId = state[action.animationId] || {};
+            const stateForAnimationType =
+                stateForAnimationId[action.animationType] || {};
+            return {
+                ...state,
+                [action.animationId]: {
+                    ...stateForAnimationId,
+                    [action.animationType]: {
+                        ...stateForAnimationType,
+                        x: action.x,
+                        y: action.y
+                    }
+                }
+            };
+        }
+        case 'animateAction':
+            return {
+                ...state,
+                [action.animationId]: {
+                    animate: true
+                }
+            };
+        case 'updateScrollAction':
+            return {
+                ...state,
+                diff: action.height - state.height,
+                height: action.height
+            };
+        default:
+            return state;
+    }
+};
+
+export default animationReducer;
