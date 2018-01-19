e8565f9d4f107f4e6c1b255c6810b5f257b0d647
Andrea Falzetti <afalzetti@gmail.com>
2018-01-19 12:05:35 +0000

    backend: cleanup

--- backend/index.js
+++ backend/index.js
@@ -23,36 +23,6 @@ const handlers = require('./lib/handlers')({ brain, config, logger });
 if (require.main === module) {
   const db = connectDb(Object.assign({}, config.db, { mongoose, logger }));
   const io = createServer(config, logger, handlers);
-  // setTimeout(async () => {
-
-  //   const questions = await brain.getQuestion({ query: { level: -1 } });
-  //   console.log('......', questions.length);
-
-  //   let i = 0;
-
-  //   const doSetTimeout = (question, i) => {
-  //     setTimeout(() => {
-  //       console.log(question.message);
-  //     }, 1000 * i);
-  //   };
-
-  //   // let i = 0;
-  //   for (question of questions) {
-  //     doSetTimeout(question, i++)
-  //   }
-  // }, 500);
-
-  // setTimeout( async () => {
-  //   const answer = await brain.getAnswerById({ query: { id: '5a60bdaf734d1d1e13025fce' } });
-  //   console.log(answer);
-
-  //   const questions = await brain.getQuestion({ query: { _id: answer.nextQuestion } });
-  //   console.log('questions to the answer', questions);
-  // }, 3000);
-  // setTimeout(async () => {
-    // const answer = await brain.getAnswerById({ query: { id: '5a60bdaf734d1d1e13025fce' } });
-  //   console.log('answer = ', answer)
-  // }, 1500);
 }
 
 module.exports = createServer;
