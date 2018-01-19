bad1d68352bd9baaff2c121adb5ad97aa9202ee7
Patrick Kunka <patrick.kunka@dazn.com>
2018-01-19 11:27:59 +0000

    integrate typing indicator into client

--- mockClient/src/index.js
+++ mockClient/src/index.js
@@ -1,17 +1,23 @@
 import ChatClient from './services/ChatClient';
-import * as Events from '../../shared/constants/Events';
+import * as Events from './constants/Events';
 
 const answersArea = document.querySelector('#answers');
 const questionArea = document.querySelector('#question');
+const buttonRestart = document.querySelector('#restart');
 
 answersArea.addEventListener('click', handleAnswerClick);
+buttonRestart.addEventListener('click', handleRestartClick);
 
 // Create new chat client with socket URL
 
-const chatClient = new ChatClient('https://a0e5f27d.ngrok.io');
+const chatClient = new ChatClient('https://e0999860.ngrok.io');
 
 function renderAnswerButtons(answers = []) {
     answersArea.innerHTML = answers.reduce((html, answer) => html + `<button data-id="${answer._id}">${answer.message}</button>`, '');
+}
+
+function renderChosenAnswer(text) {
+    questionArea.innerHTML += `<button disabled>${text}</button>`;
 }
 
 function handleAnswerClick({target}) {
@@ -19,23 +25,48 @@ function handleAnswerClick({target}) {
 
     chatClient.selectAnswer(answerId);
 
+    renderChosenAnswer(target.textContent);
     renderAnswerButtons();
+}
+
+function handleRestartClick() {
+    questionArea.innerHTML = answersArea.innerHTML = '';
+
+    chatClient.endChat();
+    chatClient.startChat();
 }
 
 // Listen for questions from the server
 
 chatClient.onQuestion(question => {
-    questionArea.value = question.message;
+    const link = question.link;
+    const typing = questionArea.querySelector('.typing');
+
+    let linkHtml = '';
+
+    if (typing) {
+        typing.parentElement.removeChild(typing);
+    }
+
+    if (link) {
+        linkHtml += `. <a href="${link}" target="_blank">Learn more</a>.`;
+    }
+
+    questionArea.innerHTML += `<li>${question.message + linkHtml}</li>`;
 
     renderAnswerButtons(question.answers);
 });
 
 chatClient.onMessage(message => {
-    questionArea.value = message;
 
-    renderAnswerButtons();
 });
 
-// Start the chat (e.g. when user opens app)
+chatClient.onTyping(message => {
+    questionArea.innerHTML += `<li class="typing">...</li>`;
+});
 
-chatClient.startChat();
+chatClient.onDisconnect(handleRestartClick);
+
+chatClient.onConnect(() => {
+    chatClient.startChat();
+});
--- mockClient/src/services/ChatClient.js
+++ mockClient/src/services/ChatClient.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
-import * as Events from '../../../shared/constants/Events';
+import * as Events from '../constants/Events';
 
 class ChatClient {
     constructor(url) {
@@ -7,6 +7,9 @@ class ChatClient {
 
         this.questionCallback = null;
         this.messageCallback = null;
+        this.connectCallback = null;
+        this.typingCallback = null;
+        this.disconnectCallback = null;
 
         this.init();
 
@@ -17,6 +20,7 @@ class ChatClient {
         this.socket.on(Events.CONNECT, this.handleConnect.bind(this));
         this.socket.on(Events.QUESTION, this.handleQuestion.bind(this));
         this.socket.on(Events.MESSAGE, this.handleMessage.bind(this));
+        this.socket.on(Events.TYPING, this.handleTyping.bind(this));
         this.socket.on(Events.DISCONNECT, this.handleDisconnect.bind(this));
     }
 
@@ -50,12 +54,28 @@ class ChatClient {
         }
     }
 
+    handleTyping() {
+        console.log('[ChatClient] Bot is typing...');
+
+        if (typeof this.typingCallback === 'function') {
+            this.typingCallback();
+        }
+    }
+
     handleConnect() {
         console.log('[ChatClient] Connected to server')
+
+        if (typeof this.connectCallback === 'function') {
+            this.connectCallback();
+        }
     }
 
     handleDisconnect() {
         console.log('[ChatClient] Server disconnected');
+
+        if (typeof this.disconnectCallback === 'function') {
+            this.disconnectCallback();
+        }
     }
 
     onQuestion(callback) {
@@ -64,6 +84,18 @@ class ChatClient {
 
     onMessage(callback) {
         this.messageCallback = callback;
+    }
+
+    onConnect(callback) {
+        this.connectCallback = callback;
+    }
+
+    onTyping(callback) {
+        this.typingCallback = callback;
+    }
+
+    onDisconnect(callback) {
+        this.disconnectCallback = callback;
     }
 
     async startChat(whoami) {
